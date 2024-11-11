95f30066a77f768f8ca07bb1461c157d647ec762
Mario <lmario35@gmail.com>
2024-11-11 10:34:23 +0100

    update port

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,8 @@
- 
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    server: {
+        port: process.env.PORT || 3000, // Utilise le port de Render ou 3000 par défaut
+        host: '0.0.0.0' // Écoute sur toutes les interfaces réseau
+    }
+}); 
